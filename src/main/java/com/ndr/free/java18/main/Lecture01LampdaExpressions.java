aad05f11485fce744ce9d1609935149546dc88a6
NA_CIFTCI <NA_CIFTCI@TN04264.thynet.thy.com>
2020-12-27 21:18:55 +0300

    Lecture01 - built-in Functional Interfaces.

--- src/main/java/com/ndr/free/java18/main/Lecture01LampdaExpressions.java
+++ src/main/java/com/ndr/free/java18/main/Lecture01LampdaExpressions.java
@@ -1,5 +1,12 @@
 package com.ndr.free.java18.main;
 
+import java.util.Comparator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.ToDoubleBiFunction;
+
 import com.ndr.free.java18.model.BiFunctionFunctionalInterface;
 import com.ndr.free.java18.model.ComsumerFunctionalInterface;
 import com.ndr.free.java18.model.FunctionFunctionalInterface;
@@ -7,10 +14,17 @@ import com.ndr.free.java18.model.SupplierFunctionalInterface;
 
 public class Lecture01LampdaExpressions {
 
+	
 	public static void main(String[] args) {
-		Runnable runnable = () -> System.out.println("Hello World!");
-		runnable.run();
 		
+		builtInFunctionalInterfaces();
+		
+		customFunctionalIntefaces();
+		
+		passingFunctionalInterface( (nValue1, nValue2) -> {return nValue1 > nValue2;} );
+	}
+	
+	private static void customFunctionalIntefaces() {
 		SupplierFunctionalInterface supplier = () -> Math.PI;
 		System.out.println(supplier.get());
 		
@@ -20,10 +34,32 @@ public class Lecture01LampdaExpressions {
 		FunctionFunctionalInterface function = (int nValue) -> nValue * nValue;
 		System.out.println(function.apply(2));
 		
-		passingFunctionalInterface( (nValue1, nValue2) -> {return nValue1 > nValue2;} );
+	}
+
+	private static void builtInFunctionalInterfaces() {
+		Runnable runnable = () -> System.out.println("Hello World!");
+		runnable.run();
+		
+		Predicate<Integer> predicate1 = nValue -> nValue > 10;
+		System.out.println(predicate1.test(3));
+		
+		Supplier<Double> supplier1 = () -> Math.PI;
+		System.out.println(supplier1.get());
+		
+		Consumer<Double> consumer1 = (x) -> System.out.println(x);
+		consumer1.accept(Math.PI);
+		
+		Comparator<String> comparator1 = (strValue1, strValue2) -> strValue1.compareTo(strValue2);
+		System.out.println(comparator1.compare("ABC", "abc"));
+		
+		Function<Double, Double> function1 = (x) -> x * x;
+		System.out.println(function1.apply((double) 3));
+		
+		ToDoubleBiFunction<Double, Double> biFunction1 = (x,y) -> x * y;
+		System.out.println(biFunction1.applyAsDouble(new Double(3), new Double(4) ));
 		
 	}
-	
+
 	private static void passingFunctionalInterface(BiFunctionFunctionalInterface biFunction){
 		System.out.println(biFunction.apply(3,5));
 	}
